90190025bafa41872587ee4d044302e068b8a8db
Mike69lyon <161336444+Mike69lyon@users.noreply.github.com>
2024-03-27 17:02:55 +0100

    pokedex test

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,20 @@
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  function PokemonCard() {
+  const pokemon = pokemonList[0] ;
+    return (<figure>
+      {pokemon.imgSrc ? (<img src={pokemon.imgSrc} />) : (<p>???</p>)}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+    ) 
+  }
+  export default PokemonCard;
